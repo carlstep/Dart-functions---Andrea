45ec3e76e304da1092a3368af967c6ca54a9167c
carlstep <>
2021-04-22 06:50:37 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,12 @@
+var name = 'Andrea';
+var age = 34;
+var height = 1.84;
+
+void main() {
+  print('hello, I\'m $name.');
+  print('My name has ${name.length} letter.');
+  print('I\'m $age years old.');
+  print('I\'m $height meters tall');
+
+  
+}
